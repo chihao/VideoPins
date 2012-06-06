5e29b92c0e204f3dc4df19fd44327542cdcc8e3d
chihao <wow.lighthead@gmail.com>
2012-06-06 11:01:12 +0800

    initial

---
+++ Chrome/background.js
@@ -0,0 +1,28 @@
+﻿
+function openWindow()
+{
+    var newWindow = new Object();
+    newWindow.url = "http://www.google.com";
+    newWindow.type = "normal";
+
+    chrome.windows.create(newWindow);
+}
+
+function plugintest()
+{
+    var plugin = document.getElementById("plugin0");
+    console.log(plugin);
+    return plugin.add(1, 5);  // call a method in your plugin
+    // return plugin.echo("This plugin seems to be working!");  // call a method in your plugin
+}
+
+/*
+function openTab(url, selected)
+{
+    var tab = new Object()
+    tab.url = url;
+	tab.selected = (typeof selected == "undefined")? false : selected;
+
+	chrome.tabs.create(tab);
+}
+*/
