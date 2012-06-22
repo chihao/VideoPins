8a8a235460412bd9e782316ec4817b6f5e69ec4b
chihao <wow.lighthead@gmail.com>
2012-06-22 11:29:24 +0800

    disable title click

--- Chrome/content_script.js
+++ Chrome/content_script.js
@@ -1,5 +1,5 @@
 
-$('#eow-title').click(function() { popup(); });
+//$('#eow-title').click(function() { popup(); });
 
 $('#watch-actions').append('<button type="button" id="VideoPins" class="yt-uix-tooltip-reverse yt-uix-button yt-uix-button-default yt-uix-tooltip" title="置頂" data-tooltip-text="置頂"><span class="yt-uix-button-content"><span class="addto-label">VideoPins</span></span></button>');
 $('#VideoPins').click(function(){
